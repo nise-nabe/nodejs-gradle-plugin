20a31202abc9615a4c081f1905d08f599d9cef15
nise_nabe <nise.nabe@gmail.com>
2022-04-02 14:01:54 +0900

    add idea-ext plugin for package prefix

--- subprojects/nodejs-gradle-plugin/build.gradle.kts
+++ subprojects/nodejs-gradle-plugin/build.gradle.kts
@@ -1,8 +1,13 @@
+import org.jetbrains.gradle.ext.settings
+import org.jetbrains.gradle.ext.packagePrefix
+
 plugins {
     java
     `maven-publish`
     `java-gradle-plugin`
     id("com.gradle.plugin-publish")
+
+    id("org.jetbrains.gradle.plugin.idea-ext")
 }
 
 pluginBundle {
@@ -16,6 +21,15 @@ gradlePlugin {
             id = "com.nisecoder.nodejs"
             implementationClass = "com.nisecoder.gradle.plugin.NodeJsPlugin"
             description = "node.js integration"
+        }
+    }
+}
+
+idea {
+    module {
+        settings {
+            packagePrefix["src/main/kotlin"] = "com.nisecoder.gradle.plugin"
+            packagePrefix["src/test/kotlin"] = "com.nisecoder.gradle.plugin"
         }
     }
 }
